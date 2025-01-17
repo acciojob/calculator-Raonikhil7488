81669e4f7df65c7309c8ed0f084cb02fd5842964
Raonikhil7488 <nikhilrao7488@gmail.com>
2025-01-17 16:13:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your code here
+let display = document.getElementById("input");
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculateResult() {
+    let expression = display.value;
+    
+    // Handle division by 0 or 0/0
+    if (expression.includes('/') && (expression.includes('0') && !expression.includes('0.'))) {
+        if (expression === '0/0') {
+            display.value = 'NaN';
+        } else {
+            display.value = 'Infinity';
+        }
+    } else {
+        try {
+            display.value = eval(expression);
+        } catch (error) {
+            display.value = "Error";
+        }
+    }
+}
+
